83cd3b92c0345460703f57581dac65c3654de5f4
Alan4101 <andriy410tolochkevitch@gmail.com>
2023-01-05 15:59:06 +0200

    update(local): added new locales

--- client/src/common/components/Modals/DeleteModal/DeleteModal.tsx
+++ client/src/common/components/Modals/DeleteModal/DeleteModal.tsx
@@ -4,19 +4,29 @@ import { Box, Button, Typography } from '@mui/material'
 import { MovieModal, MovieModalProps } from '../MainModal/MovieModal'
 
 import { RecomendedMovies } from '../../../../graphql'
+import styles from './styles'
+import { useTranslation } from 'react-i18next'
 
 interface OwnProps extends MovieModalProps {
   collection: RecomendedMovies
 }
 
 export const DeleteModal: FC<OwnProps> = props => {
+  const { t } = useTranslation()
   const { isOpen, toggleModal, collection } = props
+
   return (
     <MovieModal isOpen={isOpen} toggleModal={toggleModal}>
-      <Box>
-        <Typography>Do you realy want to delete <b> &apos;{collection.title}&apos; </b> list?</Typography>
-        <Button>Yes</Button>
-        <Button onClick={toggleModal}>No</Button>
+      <Box sx={styles.wrapper}>
+        <Typography sx={styles.text}>
+          {t('content.permitOnDeletingRecList')}
+          <b> &apos;{collection.title}&apos; </b>
+          {t('content.list')}?
+        </Typography>
+        <Button sx={styles.buttonSubmit}>{t('content.button.yes')}</Button>
+        <Button sx={styles.buttonCancel} onClick={toggleModal}>
+          {t('content.button.no')}
+        </Button>
       </Box>
     </MovieModal>
   )
